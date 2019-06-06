29cf2ecc48a1c8ab749eafc738a0c9ec2f3569f9
Raja Chakraborty <rajachakraborti@gmail.com>
2019-06-05 23:14:59 -0400

    Sample invoke of py from js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const md5 = require('md5')
+const {PythonShell} = require('python-shell')
 require('log-timestamp')
 
 const inputDir = './inputDir';
@@ -9,12 +10,33 @@ console.log('watching for file in input dir');
 let md5Previous = null;
 
 fs.watch(inputDir, (eventType, fileName) => {
-   if(fileName && eventType === 'change') {
-       const md5Current = md5(fs.readFileSync(inputDir+'/'+fileName));
-       if(md5Current === md5Previous) {
-           console.log('calling python script');
-           //call pyhton script
-       }
-       md5Previous = md5Current;
-   }
-})
+    if (fileName && eventType === 'change') {
+        const md5Current = md5(fs.readFileSync(inputDir + '/' + fileName));
+        if (md5Current === md5Previous) {
+            console.log('calling python script');
+            //callPyScript();
+            let options = {
+                mode: 'text',
+                pythonPath: '/usr/bin/python3',
+                pythonOptions: ['-u'], // get print results in real-time
+                scriptPath: './pyScripts/',
+                args: ['Raja', 'Chakraborty']
+              };
+              PythonShell.run('app.py', options, (err, results) => {
+                if(err)
+                 throw err;
+                console.log('results: %j', results) 
+            })
+        }
+        md5Previous = md5Current;
+    }
+});
+/*
+const callPyScript = () => {
+
+    const { spawn } = require('child_process')
+    const pyPrcoess = spawn('python', ['./pyScripts/app.py', 'Raja', 'Chakraborty'])
+    pyPrcoess.stdout.on('data', (data) => {
+        console.log("From Python", data.toString())
+    })
+}*/
